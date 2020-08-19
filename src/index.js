98a329edf2f92e6328e1925ba5bd8400dc4c3fc5
emddeveloper <emd.developer@gmail.com>
2020-08-19 10:00:13 +0530

    App.js component created

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Imort the React and ReactDOM
+import React from "react"
+import ReactDOM from "react-dom"
+
+//Create a react component
+const App = () => {
+  return <div>Hello !</div>
+}
+
+//Take the react component and display in screen
+ReactDOM.render(<App />, document.getElementById("root"))
