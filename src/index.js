d8228513061af40f908e5e54d5a566b9b4682c27
emddeveloper <emd.developer@gmail.com>
2020-08-19 21:13:00 +0530

    arent child component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import CommentDetails from "./CommentDetails"
+import ApprovalCard from "./ApprovalCard"
 import faker from "faker"
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:50PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
-      <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:50PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
-      <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:30PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure ?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:50PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:50PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails author={faker.name.firstName()} timeAgo="Today at 5:30PM" content="Nice Post from your bro." avatar={faker.image.avatar()} />
+      </ApprovalCard>
     </div>
   )
 }
