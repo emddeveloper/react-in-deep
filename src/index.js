b527d1bd754b871e790a09b0d30ce47baaa60f9c
emddeveloper <emd.developer@gmail.com>
2020-08-19 10:24:58 +0530

    index.js updated

--- src/index.js
+++ src/index.js
@@ -4,7 +4,12 @@ import ReactDOM from "react-dom"
 
 //Create a react component
 const App = () => {
-  return <div>Hello !</div>
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <p>Hello !</p>
+      <button style={{ backgroundColor: "blue", color: "white" }}>Submit</button>
+    </div>
+  )
 }
 
 //Take the react component and display in screen
