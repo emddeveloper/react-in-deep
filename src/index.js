95dff988a5bc5058175c220f9172aa1881358685
emddeveloper <emd.developer@gmail.com>
2020-08-20 10:32:02 +0530

    state and setState implemented

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    position => {
-      console.log(position)
-    },
-    err => {
-      console.log("Sorry")
+class App extends React.Component {
+  //js class constructor its call during class initializing
+  constructor(props) {
+    //super is the reference of parent class here React.Component
+    super(props)
+    //Initialize the state
+    this.state = {
+      latitude: null
     }
-  )
-  return <div>Hello</div>
+  }
+  //React says we have to define render() !!
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        //Update the state
+        this.setState({
+          latitude: position.coords.latitude
+        })
+      },
+      err => console.log(err)
+    )
+    return <div>Latitude:{this.state.latitude} </div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector("#root"))
