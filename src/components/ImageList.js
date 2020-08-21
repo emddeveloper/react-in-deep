beab33704b763870e5f513b87c4e69be8a525ddc
emddeveloper <emd.developer@gmail.com>
2020-08-21 12:20:42 +0530

    Alication finished lease have alook

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,10 +1,12 @@
 import React from "react"
+import "./imageList.css"
+import ImageCard from "./ImageCard"
 
 const ImageList = props => {
-  const images = props.images.map(({ id, description, urls }) => {
-    return <img src={urls.regular} key={id} alt={description}></img>
+  const images = props.images.map(image => {
+    return <ImageCard image={image} key={image.id} />
   })
-  return <div>{images}</div>
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList
