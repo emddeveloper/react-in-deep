1a6860c86671733ce109b618e08dd8ab140b444e
emddeveloper <emd.developer@gmail.com>
2020-08-21 09:47:23 +0530

    axios call with unsplash api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,33 @@
 import React from "react"
+import SearchBar from "./SearchBar"
+import axios from "axios"
 
-const App = () => {
-  return <div>Hello</div>
+class App extends React.Component {
+  state = {
+    images: []
+  }
+
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization: "Client-ID _0X2dohX4uwTSMtU1TNsow1hQczthztZpv2kvWs5P3s"
+      }
+    })
+
+    this.setState({
+      images: response.data.results
+    })
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found:{this.state.images.length} images
+      </div>
+    )
+  }
 }
 
 export default App
